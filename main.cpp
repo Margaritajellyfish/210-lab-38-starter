dc494cc7e330f7fe97272d849e8abdf65c565634
王辰毓 <margarita@MacBook-Pro-3.local>
2024-12-01 19:20:19 -0800

    1

--- main.cpp
+++ main.cpp
@@ -3,7 +3,16 @@
 using namespace std;
 
 int main() {
-    
+    IntBinaryTree tree;
+
+    cout << "Inserting nodes. ";
+    tree.insertNode(5);
+    tree.insertNode(8);
+    tree.insertNode(3);
+    tree.insertNode(12);
+    tree.insertNode(9);
+    cout << "Done.\n";
+
 
     return 0;
 }
